3c850642459a39600d6ae34b36491ada66616619
Bhargavi84 <bhargavi.parikshit@yahoo.com>
2021-04-15 16:16:58 +0000

    Update javascript for cards.js

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,6 +1,22 @@
 
-$(".readmore-btn").on("click", function(){
-    $(this).parent().toggleClass("showContent");
-    var replaceText = $(this).parent().hasClass("showContent") ? "Show less": "Show more";
-    $(this).text(replaceText);
+
+// Read more / Read less buttons
+
+
+var cards = document.querySelector('#history-cards');
+
+cards.addEventListener('click', function(e) {
+  var target = e.target; 
+  if (target.classList.contains('btn')) {       
+    var cardBody = target.parentNode;
+    var open = target.dataset.open === 'true';
+    target.dataset.open = !open;
+    if (open) {
+      target.innerText = 'Show more';
+      cardBody.classList.remove('open');
+    } else {
+      target.innerText = 'Show less';
+      cardBody.classList.add('open');
+    }
+  }  
 });
