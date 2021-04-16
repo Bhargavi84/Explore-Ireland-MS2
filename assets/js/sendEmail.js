96ffaf048a09602115289470457d347f12464a56
Bhargavi84 <bhargavi.parikshit@yahoo.com>
2021-04-16 17:41:23 +0000

    Update emailjs function

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -2,15 +2,24 @@ function sendMail(contactForm) {
     emailjs.send("gmail", "template_u6x0vpa" ,{
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+        "info_message": contactForm.message.value,
+        "newsletter_request": contactForm.newsletter.value
     })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+    then(
+            function () {           
+            Swal.fire(
+            'Thank you for contacting us!',
+            'Message sent!',
+            'success'
+            );
+        },        
+                   
+        function () {
+            Swal.fire(
+            'Oops...',
+            'Something went wrong!',
+            'error'
+            );
+        });               
+    return false;  
 }
